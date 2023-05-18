9bd0e3503e82ce06bfcaa84e8907490be08dc133
Juan Ospina <jeol3739@gmail.com>
2023-05-18 13:58:11 -0500

    Ejercicio 2 Java Intro

---
+++ src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,14 @@
+
+package ejercicio2;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+          String nombre = "Juan";
+        int num1 = 1;
+        byte num2 = 2;
+        char letra = 'J';
+        float num3 = 3;
+        double num4 = 4; 
+    }    
+}
